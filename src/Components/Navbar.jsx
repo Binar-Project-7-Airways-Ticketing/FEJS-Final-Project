dd94848f259f7a7d5e0bae5278edf9b0cef86b7a
kautsar07 <kautsar07777@gmail.com>
2022-11-24 00:57:38 +0700

    add bar search and bell notif

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../logo3.png";
 import { BsSearch } from "react-icons/bs";
-import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUser, AiOutlineBell } from "react-icons/ai";
 import "./All.css";
 import { Link } from "react-router-dom";
+import { Dropdown, Space, DatePicker, Input } from "antd";
+
 export default function Navbar() {
+  const [search, setSearch] = useState(false);
+  const srch = () => {
+    setSearch(true);
+  };
+  const srchClose = () => {
+    setSearch(false);
+  };
+  const items = [
+    {
+      label: "Selamat anda mendapatkan voucher",
+      key: "0",
+    },
+    {
+      label: "Hari ini ada promo 50% keseluruh wilayah indonesia", 
+      key: "1",
+    },
+    {
+      label: "Discount 30% class Business",
+      key: "3",
+    },
+  ];
 
   return (
     <div className="ffff">
@@ -20,22 +43,44 @@ export default function Navbar() {
             <li>Review</li>
             <li>Flight Info</li>
             <li>Contact Us</li>
-            <Link to={"/update"}>
-              <button>update</button>
-            </Link>
           </ul>
 
           <div className="search">
-            <BsSearch />
-            <AiOutlineUser />
-            <a href="/login">Login</a>
-            <p style={{display:"flex", justifyContent:"center", margin:"0"}}>|</p>
-            <a>Sign up</a>
-            <p>|</p>
-            <a href="/register">Sign up</a>
+            {search ? (
+              <Input placeholder="Basic usage" />
+            ) : (
+              <Input className="frm" placeholder="Basic usage" />
+            )}
+            {search ? (
+              <BsSearch onClick={srchClose} />
+            ) : (
+              <BsSearch onClick={srch} />
+            )}
+
+            <div className="notif">
+              <Dropdown
+                menu={{
+                  items,
+                }}
+                trigger={["click"]}
+              >
+                <a onClick={(e) => e.preventDefault()}>
+                  <Space>
+                    <AiOutlineBell style={{ fontSize: "20px" }} />
+                    <p>Notifikasi</p>
+                  </Space>
+                </a>
+              </Dropdown>
+            </div>
+            <div className="user">
+              <AiOutlineUser style={{fontSize:"20px"}}/>
+              <a href="/login">Login</a>
+              <p>|</p>
+              <a href="/register">Sign up</a>
+            </div>
           </div>
         </div>
       </div>
-    </div> 
-  ); 
+    </div>
+  );
 }
