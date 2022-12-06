a7639eaf98be15d3d7e262d5471153b0a55b5625
kautsar07 <kautsar07777@gmail.com>
2022-12-06 21:10:09 +0700

    add benefit section

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Bookingpage from "./Components/Pages/Bookingpage";
 import Profile from "./Components/Pages/Profile";
 import Transaction from "./Components/Transaction";
 import FlightStatusPage from "./Components/Pages/FlightStatusPage";
+import axios from "axios";
 
 function App() {
   return (
@@ -26,3 +27,21 @@ function App() {
 }
 
 export default App;
+axios.interceptors.request.use(function (config) {
+  console.log(config);
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
+});
