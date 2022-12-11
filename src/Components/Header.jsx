18fd2c298f47bc9683de607a5541bb226eb49c8c
kautsar07 <kautsar07777@gmail.com>
2022-12-11 11:27:55 +0700

    responsive header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
           <div className="header-main">
             <div className="slogan">
               <h1>Let's go to the best places in the World with us</h1>
-              <p style={{ fontSize: "1rem" }}>
+              <p >
                 Would you explore nature paradise in the world, let't find the
                 best destination in world with us.
               </p>
