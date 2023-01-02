0e119e38181ef1d54c00226c35f010a27b699a9b
lusipr <lusipr142@gmail.com>
2023-01-02 13:38:16 +0700

    merge

--- src/Components/Pages/Login.test.js
+++ src/Components/Pages/Login.test.js
@@ -1,14 +1,17 @@
-// test('there is no I in team', () => {
-//     expect('team').not.toMatch(/I/);
-// })
-import { render } from "@testing-library/react"
+import { render } from "@testing-library/react";
 import Login from "./Login";
 
 describe("unit test", () => {
-    it("test", () => {
-        const { getByTestId } = render(<Login />);
-        const buttonClick = getByTestId("test-button");
-        // console.log("buttonClick".buttonClick)
-        expect(buttonClick).toBeTruthy();
-    })
-})
+  test("username input should be render", () => {
+    const { getByPlaceholderText } = render(<Login />);
+    const userInput = screen.getByPlaceholderText(/Email/i);
+    expect(userInput).toBeInTheDocument();
+  });
+
+  it("test", () => {
+    const { getByTestId } = render(<Login />);
+    const buttonClick = getByTestId("test-button");
+    // console.log("buttonClick".buttonClick)
+    expect(buttonClick).toBeTruthy();
+  });
+});
